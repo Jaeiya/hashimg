b3905f3acc6e4eeb918292b6169b4c0b7a72123b
Jaeiya <exanix@gmail.com>
2024-08-09 13:45:53 -0700

    feat: add windows build script

---
+++ build.bat
@@ -0,0 +1,13 @@
+@echo off
+REM Set the target architecture and operating system
+set GOARCH=amd64
+set GOOS=windows
+set CGO_ENABLED=0
+
+REM Build the Go application with optimization flags
+go build -ldflags="-s -w" -gcflags="all=-B -l=120" -trimpath -o "bin/hashimg.exe" "cmd/hashimg/main.go"
+
+REM Optionally, you can specify the output binary name
+REM go build -o myapp -ldflags="-s -w" -gcflags="-B" -trimpath
+
+echo Build completed with optimizations.
